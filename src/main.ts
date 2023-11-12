26cacee8bc5eda8812f448361a210507cbe6d929
Egor Dovgalev <egor.dovgalev.anthill@gmail.com>
2023-11-12 19:19:57 +0300

    я устал

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.enableCors();
+  app.enableCors({
+    origin:'/*',
+    credentials:true,
+    optionsSuccessStatus:200
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
