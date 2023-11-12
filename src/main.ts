dc4de7d60d828d61328894bf62e709683eb689bf
Egor Dovgalev <egor.dovgalev.anthill@gmail.com>
2023-11-12 19:34:42 +0300

    fix 1

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{ cors: true });
   app.setGlobalPrefix('api');
-  app.enableCors({
-    origin:'*',
-    credentials:true,
-    optionsSuccessStatus:200
-  });
-  await app.listen(process.env.PORT || 3000);
+  app.enableCors();
+  await app.listen(4445);
 }
 bootstrap();
