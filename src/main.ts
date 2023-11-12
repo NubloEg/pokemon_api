a3d88858965380e5a5205f092d26431a0c58097e
Egor Dovgalev <egor.dovgalev.anthill@gmail.com>
2023-11-12 20:46:10 +0300

    da

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{ cors: true });
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
  app.enableCors({
     origin: true, // Разрешаем доступ только с этого домена
