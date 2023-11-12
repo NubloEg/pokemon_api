56e92d60c8ed169304c73eb19273fad110ebce77
Egor Dovgalev <egor.dovgalev.anthill@gmail.com>
2023-11-12 20:39:48 +0300

    xsa

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{ cors: true });
   app.setGlobalPrefix('api');
-  app.enableCors({
-    origin:'https://pokemon-game-delta.vercel.app/auth',
-    credentials:true
+ app.enableCors({
+    origin: ['https://pokemon-game-delta.vercel.app/auth','http://localhost:3000'], // Разрешаем доступ только с этого домена
+    credentials: true, // Разрешаем отправку куки
+    methods: 'GET,POST', // Разрешаем отправку только этих методов
+    allowedHeaders: 'Content-Type,Authorization', // Разрешаем только указанные заголовки
   });
   await app.listen(4445);
 }
